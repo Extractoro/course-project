0155c660d994fa74b8675cdd30857f1ea3da9105
Extractoro <vadymtytarenkoo@gmail.com>
2024-10-06 15:26:09 +0300

    add: auth forget password & reset password

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -23,13 +23,14 @@ router.post('/registration', controllersWrapper((req: Request, res: Response) =>
             })
         }
 
-        const sqlQuery = `CALL registrationUser(?, ?, ?, ?, ?, ?)`;
+        const sqlQuery = `CALL registrationUser(?, ?, ?, ?, ?, ?, ?)`;
         const {firstName, lastName, email, password, phone} = req.body
 
         const hashedPassword = await bcrypt.hash(password, 10);
         const verificationToken = uuidv4()
+        const resetPasswordToken = uuidv4()
 
-        connection.query(sqlQuery, [firstName, lastName, email, hashedPassword, phone || null, verificationToken],function (err, results) {
+        connection.query(sqlQuery, [firstName, lastName, email, hashedPassword, phone || null, verificationToken, resetPasswordToken],function (err, results) {
             if (err) {
                 connection.release();
                 return res.status(400).send({
@@ -262,130 +263,305 @@ router.get("/registration_confirm/:verificationToken", controllersWrapper((req: 
                 })
             })
         })
-    }
-))
+    }))
 
 router.get("/confirmation_resend", controllersWrapper((req: Request, res: Response) => {
-        database.getConnection(function (err, connection) {
-            if (err) {
-                console.log(err);
-                return res.status(500).send({
-                    status: 500,
+    database.getConnection(function (err, connection) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send({
+                status: 500,
+                success: false,
+                message: err.message,
+            })
+        }
+
+        const { email: emailReceiver } = req.body;
+
+        const sqlSelect = `SELECT user_firstname, user_lastname, email, verificationToken FROM users WHERE email = ? AND verify = 0`;
+
+        connection.query(sqlSelect, [emailReceiver], async function (err, rows) {
+            if (err || rows?.length === 0) {
+                connection.release();
+                return res.status(400).send({
+                    status: 400,
                     success: false,
-                    message: err.message,
+                    message: "Something went wrong or you are already verified. Please write to support!",
                 })
             }
 
-            const { email: emailReciever } = req.body;
+            res.status(200).send({
+                status: 200,
+                success: true,
+                message: 'Successfully sent. Now confirm your email!',
+            })
 
-            const sqlSelect = `SELECT user_firstname, user_lastname, email, verificationToken FROM users WHERE email = ? AND verify = 0`;
+            console.log(rows[0])
+            const { user_firstname, user_lastname, email, verificationToken } = rows[0];
 
-            connection.query(sqlSelect, [emailReciever], async function (err, rows) {
-                if (err || rows?.length === 0) {
-                    connection.release();
+            await transporter.sendMail({
+                to: email,
+                from: "EventNest <vadym.tytarenko@nure.ua>",
+                subject: "Confirm your email for EventNest",
+                html: `<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Email Confirmation - EventNest</title>
+                    <style>
+                        body {
+                            font-family: Arial, sans-serif;
+                            background-color: #f4f4f4;
+                            margin: 0;
+                            padding: 0;
+                        }
+                        .container {
+                            width: 100%;
+                            padding: 20px;
+                            background-color: #f4f4f4;
+                        }
+                        .content {
+                            max-width: 600px;
+                            margin: 0 auto;
+                            background-color: #ffffff;
+                            padding: 20px;
+                            border-radius: 8px;
+                            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+                        }
+                        .header {
+                            text-align: center;
+                            margin-bottom: 20px;
+                        }
+                        .header h1 {
+                            color: #333;
+                        }
+                        .button {
+                            display: inline-block;
+                            color: white !important;;
+                            background-color: #1abc9c; 
+                            padding: 10px 20px;
+                            text-decoration: none;
+                            border-radius: 5px;
+                            font-size: 16px;
+                            transition: background-color 0.8s ease;
+                        }
+                        .button:hover {
+                            background-color: #16a085;
+                        }
+                        .footer {
+                            text-align: center;
+                            margin-top: 20px;
+                            color: #777;
+                            font-size: 12px;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <div class="container">
+                        <div class="content">
+                            <div class="header">
+                                <h1>Confirm Your Email Address</h1>
+                            </div>
+                            <p>Hello, ${user_firstname} ${user_lastname}!</p>
+                            <p>Thank you for registering with <strong>EventNest</strong> — your portal for booking event tickets. To complete the registration process and activate your account, please confirm your email address by clicking the button below.</p>
+                            <div style="text-align: center; margin: 20px 0;">
+                                <a href="http://localhost:3000/auth/registration_confirm/${verificationToken}" class="button">Confirm Email</a>
+                            </div>
+                            <p>If the button doesn't work, copy and paste the following URL into your browser's address bar:</p>
+                            <p>http://localhost:3000/auth/registration_confirm/${verificationToken}</p>
+                            <p>If you did not register for EventNest, you can safely ignore this email.</p>
+                            <p>Best regards,<br>The EventNest Team</p>
+                        </div>
+                        <div class="footer">
+                            <p>&copy; 2024 EventNest. All rights reserved.</p>
+                        </div>
+                    </div>
+                </body>
+                </html>
+                `,
+            })
+        })
+    })
+}))
+
+router.post("/forget_password", controllersWrapper((req: Request, res: Response) => {
+    database.getConnection(function (err, connection) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send({
+                status: 500,
+                success: false,
+                message: err.message,
+            })
+        }
+
+        const { email: emailReceiver } = req.body;
+
+        if (!emailReceiver) {
+            return res.status(400).send({
+                status: 400,
+                success: false,
+                message: "Email is required.",
+            });
+        }
+
+        const sqlSelect = `SELECT user_firstname, user_lastname, email, resetPasswordToken FROM users WHERE email = ?`;
+
+        connection.query(sqlSelect, [emailReceiver], async function (err, rows) {
+            if (err || rows?.length === 0) {
+                connection.release();
+                return res.status(400).send({
+                    status: 400,
+                    success: false,
+                    message: "Something went wrong or you have not even registered yet. Please write to support!",
+                })
+            }
+
+            res.status(200).send({
+                status: 200,
+                success: true,
+                message: 'Successfully sent. You can change your password!',
+            })
+
+            const { user_firstname, user_lastname, email, resetPasswordToken } = rows[0];
+
+            await transporter.sendMail({
+                to: email,
+                from: "EventNest <vadym.tytarenko@nure.ua>",
+                subject: "Change your password",
+                html: `<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Reset Password - EventNest</title>
+                    <style>
+                        body {
+                            font-family: Arial, sans-serif;
+                            background-color: #f4f4f4;
+                            margin: 0;
+                            padding: 0;
+                        }
+                        .container {
+                            width: 100%;
+                            padding: 20px;
+                            background-color: #f4f4f4;
+                        }
+                        .content {
+                            max-width: 600px;
+                            margin: 0 auto;
+                            background-color: #ffffff;
+                            padding: 20px;
+                            border-radius: 8px;
+                            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+                        }
+                        .header {
+                            text-align: center;
+                            margin-bottom: 20px;
+                        }
+                        .header h1 {
+                            color: #333;
+                        }
+                        .button {
+                            display: inline-block;
+                            background-color: #1abc9c;
+                            color: white !important;
+                            padding: 10px 20px;
+                            text-decoration: none;
+                            border-radius: 5px;
+                            font-size: 16px;
+                            transition: background-color 0.3s ease; /* Плавный переход цвета */
+                        }
+                        .button:hover {
+                            background-color: #16a085; /* Темнее при наведении */
+                        }
+                        .footer {
+                            text-align: center;
+                            margin-top: 20px;
+                            color: #777;
+                            font-size: 12px;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <div class="container">
+                        <div class="content">
+                            <div class="header">
+                                <h1>Reset Your Password</h1>
+                            </div>
+                            <p>Hello, ${user_firstname} ${user_lastname}!</p>
+                            <p>To reset your password, please click the button below:</p>
+                            <div style="text-align: center; margin: 20px 0;">
+                                <a href="http://localhost:3000/auth/reset_password/${resetPasswordToken}" class="button">Reset Password</a>
+                            </div>
+                            <p>If you did not request a password reset, please ignore this email.</p>
+                            <p>Best regards,<br>The EventNest Team</p>
+                        </div>
+                    </div>
+                </body>
+                </html>
+                `,
+            })
+        })
+    })
+}))
+
+router.post("/reset_password/:resetPasswordToken", controllersWrapper( (req: Request, res: Response) => {
+    const { resetPasswordToken } = req.params;
+    const { newPassword } = req.body;
+
+    if (!newPassword) {
+        return res.status(400).send({
+            status: 400,
+            success: false,
+            message: "New password is required.",
+        });
+    }
+
+    database.getConnection(async function (err, connection) {
+        if (err) {
+            return res.status(500).send({
+                status: 500,
+                success: false,
+                message: err.message,
+            });
+        }
+
+        const sqlUpdate = `UPDATE users SET password = ? WHERE resetPasswordToken = ?`;
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        if (!hashedPassword) {
+            return res.status(500).send({
+                status: 500,
+                success: false,
+                message: "Something went wrong. Please write to support!",
+            });
+        }
+
+        connection.query(sqlUpdate, [hashedPassword, resetPasswordToken], function (err, result) {
+                connection.release();
+
+                if (err || result.affectedRows === 0) {
                     return res.status(400).send({
                         status: 400,
                         success: false,
-                        message: "Something went wrong or you are already verified. Please write to support!",
-                    })
+                        message: "Invalid token or something went wrong.",
+                    });
                 }
 
                 res.status(200).send({
                     status: 200,
                     success: true,
-                    message: 'Successfully sent. Now confirm your email!',
-                })
+                    message: 'Password has been reset successfully!',
+                });
+            });
+        });
+}));
 
-                console.log(rows[0])
-                const { user_firstname, user_lastname, email, verificationToken } = rows[0];
-
-                await transporter.sendMail({
-                    to: email,
-                    from: "EventNest <vadym.tytarenko@nure.ua>",
-                    subject: "Confirm your email for EventNest",
-                    html: `<!DOCTYPE html>
-                    <html lang="en">
-                    <head>
-                        <meta charset="UTF-8">
-                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <title>Email Confirmation - EventNest</title>
-                        <style>
-                            body {
-                                font-family: Arial, sans-serif;
-                                background-color: #f4f4f4;
-                                margin: 0;
-                                padding: 0;
-                            }
-                            .container {
-                                width: 100%;
-                                padding: 20px;
-                                background-color: #f4f4f4;
-                            }
-                            .content {
-                                max-width: 600px;
-                                margin: 0 auto;
-                                background-color: #ffffff;
-                                padding: 20px;
-                                border-radius: 8px;
-                                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-                            }
-                            .header {
-                                text-align: center;
-                                margin-bottom: 20px;
-                            }
-                            .header h1 {
-                                color: #333;
-                            }
-                            .button {
-                                display: inline-block;
-                                color: white !important;;
-                                background-color: #1abc9c; 
-                                padding: 10px 20px;
-                                text-decoration: none;
-                                border-radius: 5px;
-                                font-size: 16px;
-                                transition: background-color 0.8s ease;
-                            }
-                            .button:hover {
-                                background-color: #16a085;
-                            }
-                            .footer {
-                                text-align: center;
-                                margin-top: 20px;
-                                color: #777;
-                                font-size: 12px;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <div class="container">
-                            <div class="content">
-                                <div class="header">
-                                    <h1>Confirm Your Email Address</h1>
-                                </div>
-                                <p>Hello, ${user_firstname} ${user_lastname}!</p>
-                                <p>Thank you for registering with <strong>EventNest</strong> — your portal for booking event tickets. To complete the registration process and activate your account, please confirm your email address by clicking the button below.</p>
-                                <div style="text-align: center; margin: 20px 0;">
-                                    <a href="http://localhost:3000/auth/registration_confirm/${verificationToken}" class="button">Confirm Email</a>
-                                </div>
-                                <p>If the button doesn't work, copy and paste the following URL into your browser's address bar:</p>
-                                <p>http://localhost:3000/auth/registration_confirm/${verificationToken}</p>
-                                <p>If you did not register for EventNest, you can safely ignore this email.</p>
-                                <p>Best regards,<br>The EventNest Team</p>
-                            </div>
-                            <div class="footer">
-                                <p>&copy; 2024 EventNest. All rights reserved.</p>
-                            </div>
-                        </div>
-                    </body>
-                    </html>
-                    `,
-                })
-            })
-        })
-    }
-))
 
 router.post('/login', controllersWrapper((req: Request, res: Response) => {
     database.getConnection( function(err, connection) {
